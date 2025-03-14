dde6d78109869dc2401098a47626e03167b0c9c2
Juan Daniel Guerrero Canchola <yodanstack@gmail.com>
2025-03-13 20:12:33 -0600

    actualizacion (seccion 5)

--- api/app.js
+++ api/app.js
@@ -13,7 +13,19 @@ const app = express();
     app.use(bodyParser.urlencoded({extended:false}));
     app.use(bodyParser.json());
 
-//
+// rutas
+app.get('/',(req, resp) => {
+    resp.status(200).send({
+        message: '........'
+    });
+});
+
+
+app.get('/pruebas',(req, resp) => {
+    resp.status(200).send({
+        message: 'accion de pruebas en el servidor de nodeJs'
+    });
+});
 
 //Exportar
 module.exports = app;
