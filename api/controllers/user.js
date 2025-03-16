a14291db20d700da0c974906b813397b56a24ce3
Juan Daniel Guerrero Canchola <yodanstack@gmail.com>
2025-03-15 20:02:01 -0600

    actializacion sesion 1

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,9 +2,15 @@
 
 
 const bcrypt = require('bcrypt-nodejs');
-const User = require('../models/user');
-const jwt = require('../services/jwt')
+const mongopag = require('mongoose-pagination');
+const fs = require('fs');
+const path = require('path');
 
+const User = require('../models/user');
+const jwt = require('../services/jwt');
+const user = require('../models/user');
+
+//meotodos de prueba
 function home(req, resp){
     resp.status(200).send({
         message: '........'
@@ -75,7 +81,8 @@ function loginUser(req, resp){
     
         const email = params.email;
         const password = params.password;
-    user.findOne({email: emaild}, (err, user) => {
+        
+        user.findOne({email: emaild}, (err, user) => {
         if(err) resp.status(500).send({message: 'Error en la oeticion'});
 
         if(user) {
@@ -102,10 +109,117 @@ function loginUser(req, resp){
             }
     });
 }
+
+//metodo para conseguir datos de un usuario 
+    function getUser(req, resp) {
+        const userId = req.params.id;
+
+        user.findById(userId, (err, user)=> {
+            if(err) return resp.status(500).send({message: 'error en la peticion'});
+
+            if(!user) return resp.status(404).send({message: 'usuario no existe'});
+            
+            return resp.status(200).send({user});
+        });
+    }
+
+    //devolver usuarios ingresados 
+    function getUsers(req, resp){
+        const identityUserId = req.user.sub;
+        let page = 1;
+
+        if(req.params.page){
+            page = req.params.page;
+        }
+        const itemsPerPage = 5;
+
+        User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total)=> {
+         
+            if(err){
+                return resp.status(500).send({message: 'error en la peticion'});
+            }
+            if(!user) {
+            return resp.status(404).send({message: 'No hay usuarios para monstrar'});
+            }
+        
+            return resp.status(200).send({
+                users,
+                total,
+                page: math.ceil(total/itemsPerPage)
+            });
+        });
+
+    }
+
+// Edicion de datos de usuario
+    function updateUser(req, resp){
+        const userId = req.params.id;
+        const update = req.body;
+        
+        //borrar password
+        delete update.password;
+
+        if(userId != req.user.sub){
+            return resp.status(500).send({
+                message: 'no tienes permiso para actualizar datos'
+            });
+        }
+        User.findByIdAndUpdate(userId, update, {new: true}, (err, userUpdate)=> {
+            if(err){
+                return resp.status(500).send({message: 'error en la peticion'});
+            }
+           if(!userUpdate) return resp.status(404).send({
+            message: 'No se ha podido actualizar el usuario'
+           });
+           return resp.status(200).send({userUpdate}); 
+        });
+    }
+
+    //subir archivos de imagen / avatar de usuario
+    function uploadImage(req, resp) {
+        const userId = req.params.id;
+
+        if(userId != req.user.sub){
+            return resp.status(500).send({
+                message: 'No tienes permisos para aletar este perfil'
+            });
+            if(req.files){
+                const filePath = req.files.image.path;
+                const file_split = filePath.split('\\');
+                // obtiene el nombre de la imagen
+                const file_name = file_split[2]; 
+                // devolver la extencion de la imagen ingresada
+                const ext_split = file_name.split('\.');
+                //arroja unicamente la extencion de la imagen
+                const file_ext = ext_split[1];
+            
+                if(file_ext === 'png' || file_ext === 'jpg' ||
+                   file_ext === 'jpeg' || file_ext === 'gif'){
+                    //actualizar documento de usuario 
+
+                }else{
+                    fs.unlink(file_path, (err)=> {
+                        if(err) return resp.status(200).send({
+                            message: 'Extencion no valida'
+                        });
+                    });
+                }
+            }
+        }else{
+            return resp.status(200).send({
+                message: 'No se han subido imagenes'
+            });
+        }
+    }
+ 
     
 module.exports = {
     home,
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    getUser,
+    getUsers,
+    updateUser,
+    uploadImage
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const userController = require('../controllers/user');
 const md_auth = require('../middlewares/authenticated');
+const multipart = require('connect-multiparty');
+const md_upload = multipart({uploadDir: './uploads/users'});
 
 const api = express.Router();
 
@@ -10,6 +12,9 @@ api.get('/home', userController.home);
 api.get('/pruebas', md_auth.ensureAuth, userController.pruebas);
 api.post('/register', userController.saveUser);
 api.post('/loginUser',userController.loginUser);
-
+api.get('/user/:id', md_auth.ensureAuth,userController.getUser);
+api.get('/users/:page?', md_auth.ensureAuth, userController.getUsers);
+api.put('/update-user/:id', md_auth.ensureAuth, userController.updateUser);
+api.post('/upload-image-user/:id' [md_auth.ensureAuth, md_upload], userController.uploadImage);
 
 module.exports = api;
